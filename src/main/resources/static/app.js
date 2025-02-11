2d5071e33577a1fa850acc96ef0eb7184b626647
tarosuke <felis.silvestris.catus.7777@gmail.com>
2025-02-11 15:29:59 +0900

    feat: change stomp to websocketapi

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,56 +1,29 @@
-const stompClient = new StompJs.Client({
-    brokerURL: 'ws://localhost:8080/gs-guide-websocket'
-});
-
-stompClient.onConnect = (frame) => {
-    setConnected(true);
-    console.log('Connected: ' + frame);
-    stompClient.subscribe('/topic/message', (message) => {
-        showMessage(JSON.parse(message.body));
-    });
-};
-
-stompClient.onWebSocketError = (error) => {
-    console.error('Error with websocket', error);
-};
-
-stompClient.onStompError = (frame) => {
-    console.error('Broker reported error: ' + frame.headers['message']);
-    console.error('Additional details: ' + frame.body);
-};
-
-function setConnected(connected) {
-    if (connected) {
-        $("#jsiConversation").show();
-    }
-    else {
-        $("#jsiConversation").hide();
-    }
-}
-
-function connect() {
-    stompClient.activate();
-}
-
-function disconnect() {
-    stompClient.deactivate();
-    setConnected(false);
-    console.log("Disconnected");
-}
-
-function sendName() {
-    stompClient.publish({
-        destination: "/app/message",
-        body: JSON.stringify({'content': $("#jsiContent").val(), 'channelId':$("#jsiChannelId").val()})
-    });
-	$("#jsiContent").val("");
-}
-
-function showMessage(message) {
-	$("#messages").append("<tr><td>"+ message.createdAt + "</td><td>" + message.content + "</td></tr>");
-}
+// https://developer.mozilla.org/ja/docs/Web/API/WebSockets_API/Writing_WebSocket_client_applications
 
 $(function () {
-	connect();
-    $( "#jsiSend" ).click(() => sendName());
+	
+	const socket = new WebSocket('ws://localhost:8080/hc-websocket?' + $("#jsiChannelId").val());;
+
+	socket.onmessage = (message) => {
+		  showMessage(JSON.parse(message.data));
+	};
+
+	function sendName() {
+		const msg = {
+		  content: $("#jsiContent").val(),
+		  channelId: $("#jsiChannelId").val()
+		};
+		
+		socket.send(JSON.stringify(msg));
+		$("#jsiContent").val("");
+	}
+
+	function showMessage(message) {
+		$("#messages").append("<tr><td>"+ message.createdAt + "</td><td>" + message.content + "</td></tr>");
+	}
+	
+	$( "#jsiSend" ).click(() => sendName());
+	
 });
+
+// https://stackoverflow.com/questions/10503606/scroll-to-bottom-of-div-on-page-load-jquery
