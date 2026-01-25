33eaf769a5c733debb023bbe0217ba4a123d5364
tarosuke <felis.silvestris.catus.7777@gmail.com>
2026-01-25 20:55:26 +0900

    MessageHandlerを自動コンポーネントに統一

--- src/main/java/com/tarosuke777/hc/config/WebSocketConfig.java
+++ src/main/java/com/tarosuke777/hc/config/WebSocketConfig.java
@@ -1,9 +1,8 @@
 package com.tarosuke777.hc.config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistration;
@@ -14,16 +13,15 @@ import com.tarosuke777.hc.handler.MessageHandler;
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
 
+	@Autowired
+	private MessageHandler messageHandler;
+
 	@Override
 	public void registerWebSocketHandlers(@NonNull WebSocketHandlerRegistry registry) {
 
-		WebSocketHandlerRegistration registration = registry.addHandler(handler(), "/hc-websocket");
+		WebSocketHandlerRegistration registration =
+				registry.addHandler(messageHandler, "/hc-websocket");
 		registration.setAllowedOrigins("*");
-	}
-
-	@Bean
-	public WebSocketHandler handler() {
-		return new MessageHandler();
 	}
 
 }
