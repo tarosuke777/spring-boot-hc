954067ca1f9a206c01fd77ea49c3c49c43429702
tarosuke <felis.silvestris.catus.7777@gmail.com>
2026-04-05 19:43:54 +0900

    不要なインポートを削除

--- src/main/java/com/tarosuke777/hc/controller/MessageController.java
+++ src/main/java/com/tarosuke777/hc/controller/MessageController.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
