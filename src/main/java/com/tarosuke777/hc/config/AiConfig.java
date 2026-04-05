09aaaa26925ff0a316d1c61f5d7b4d3e9efac457
tarosuke <felis.silvestris.catus.7777@gmail.com>
2026-04-05 18:25:43 +0900

    不要なインポートを削除

--- src/main/java/com/tarosuke777/hc/config/AiConfig.java
+++ src/main/java/com/tarosuke777/hc/config/AiConfig.java
@@ -3,7 +3,6 @@ package com.tarosuke777.hc.config;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
-import org.springframework.ai.ollama.api.OllamaChatOptions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
