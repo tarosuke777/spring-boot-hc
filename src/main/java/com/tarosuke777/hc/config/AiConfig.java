7ba1175da0953131af8539e07bbc446f739156e4
tarosuke <felis.silvestris.catus.7777@gmail.com>
2026-04-07 20:38:11 +0900

    RAGを追加

--- src/main/java/com/tarosuke777/hc/config/AiConfig.java
+++ src/main/java/com/tarosuke777/hc/config/AiConfig.java
@@ -1,8 +1,14 @@
 package com.tarosuke777.hc.config;
 
+import java.util.List;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,9 +20,24 @@ public class AiConfig {
     ChatMemory chatMemory;
 
     @Bean
-    public ChatClient chatClient(ChatClient.Builder builder) {
+    public VectorStore vectorStore(EmbeddingModel embeddingModel) {
+        SimpleVectorStore store = SimpleVectorStore.builder(embeddingModel).build();
+
+        List<Document> documents = List.of(new Document("tarosuke777の趣味は、開発です。"),
+                new Document("Home Chatプロジェクトは、JavaとSpring AIで構築されています。"));
+        store.add(documents);
+
+        return store;
+    }
+
+    @Bean
+    public ChatClient chatClient(ChatClient.Builder builder, VectorStore vectorStore) {
         return builder
                 .defaultSystem("あなたは関西出身のフレンドリーなアシスタントです。回答はすべて、自然な関西弁で行ってください。挨拶は抜きで、簡潔に頼みます。")
-                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build()).build();
+                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build(),
+                        QuestionAnswerAdvisor.builder(vectorStore).build())
+                .build();
     }
+
+
 }
